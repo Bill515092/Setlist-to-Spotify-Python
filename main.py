498715a65c5f062506a0d63ad87df3c4ce8bac07
John Doe <johndoe@example.com>
2025-04-18 14:54:45 +0100

    Track search updated to accept multiple tracks, and playlist add is now working and can add however many tracks passed through.

--- main.py
+++ main.py
@@ -20,9 +20,9 @@ auth_code = input("Paste the authorization code from URL: ")
 
 
 band_name = "The Story So Far"
-setlist = ["Small Talk", "Quicksand", "Framework"]
+setlist = ["Small Talk", "Quicksand", "Framework", "Empty Space", "Roam", "Navy Blue"]
 id_arr = []
-
+uri_arr = []
 
 def get_token(auth_code):
     auth_string = client_id + ":" + client_secret
@@ -102,18 +102,26 @@ def search_item(token, band_name, setlist):
         
         data = response.json()
         items = data.get("tracks").get("items")
-        id_arr.append(items[0]["id"])
- 
+
+        if items[0]["name"] == i:
+            id_arr.append(items[0]["id"])
+        else:
+            "We are unable to find this song. Please select another."
+
     return id_arr
     
 
 # Create a function to search for a list of songs add them to an array (Spotify URI's). Pass the found spotift ID's
-def search_tracks(token, spotify_id):
+def search_tracks(token, spotify_ids):
+    
+    spotify_ids = ",".join(spotify_ids)
+    print(spotify_ids)
+
     url = "https://api.spotify.com/v1/tracks"
     headers = get_auth_header(token)
     params = {
         "market": "UK",
-        "ids": spotify_id  
+        "ids": spotify_ids
     }
     response = requests.get(url, headers=headers, params=params)
 
@@ -122,21 +130,26 @@ def search_tracks(token, spotify_id):
         return None
     
     data = response.json()
-
-    return data.get("tracks")[0]["uri"]
+    index = 0
+    while len(uri_arr) < len(id_arr):
+        uri = data.get("tracks")[index]["uri"]
+        uri_arr.append(uri)
+        index += 1
+    
+    return uri_arr
     
 # Create a function to add items to a playlist, and pass the song array variable in the bost of the request.
-def add_to_playlist(token, spotify_uri, playlist_id):
+def add_to_playlist(token, spotify_uris, playlist_id):
+
     url = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks"
     headers = get_auth_header(token)
     body = {
-        "uris": [spotify_uri],
+        "uris": spotify_uris,
         "position": 0
     }
     data = json.dumps(body)
-
     response = requests.post(url, headers=headers, data=data)
-
+    
     if response.status_code != 201:
         print("Error fetching user info (Create Playlist):", response.json()) 
         return None
@@ -154,15 +167,13 @@ else:
     print("Failed to retrieve token.")
 
 search_item(token, band_name, setlist)
-# print(spotify_id)
-print(id_arr)
+# print(id_arr)
 
-# spotify_uris = search_tracks(token, spotify_id)
-# print(spotify_uris)
+search_tracks(token, id_arr)
+# print("uri array =", uri_arr)
 
-# playlist_id = create_playlist(token, user_id)
+playlist_id = create_playlist(token, user_id)
 # print(playlist_id)
 
-search_item(token, band_name, setlist)
 # create_playlist(token, user_id)
-# add_to_playlist(token, spotify_uris, playlist_id)
+add_to_playlist(token, uri_arr, playlist_id)
